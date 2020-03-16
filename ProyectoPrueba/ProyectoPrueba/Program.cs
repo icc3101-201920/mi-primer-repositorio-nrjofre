80338ecfee8db17b0a605c8f1f4d7c19f03d826f
Nicolás Jofré <nrjofre@miuandes.cl>
2020-03-16 14:32:20 -0300

    segundo comit

--- ProyectoPrueba/ProyectoPrueba/Program.cs
+++ ProyectoPrueba/ProyectoPrueba/Program.cs
@@ -7,6 +7,7 @@ namespace ProyectoPrueba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola");
         }
     }
 }
